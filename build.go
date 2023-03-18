ca2acedd28bb648e68f8544b5e8cd7d555f81cb5
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2023-03-18 10:01:58 -0400

    Kbuild: add clean cmdline flag

--- build.go
+++ build.go
@@ -1,5 +1,7 @@
 package main
 
+import "os"
+
 func main() {
 	println("[kbuild]")
 
@@ -7,6 +9,17 @@ func main() {
 	arch := getArch(config)
 	toolchain := getToolChain(arch, config)
 
-	objs := Rake(arch.GetArchPath(), config, toolchain, Build)
-	toolchain.Link(".", objs, "vminix")
+	var action Action
+
+	if len(os.Args) == 1 {
+		action = Build
+	} else if os.Args[1] == "clean" {
+		action = Clean
+	}
+
+	objs := Rake(arch.GetArchPath(), config, toolchain, action)
+
+	if action == Build {
+		toolchain.Link(".", objs, "vminix")
+	}
 }
